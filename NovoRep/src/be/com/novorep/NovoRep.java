5f6c276569ad8596d0335cd08997c965cd2a7c85
filipehcds <filipehcds.sp@gmail.com>
2014-06-26 19:48:47 -0300

    Comitttt firstttt =]

---
+++ NovoRep/src/be/com/novorep/NovoRep.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package be.com.novorep;
+
+/**
+ * @author Filipehcds
+ *
+ */
+public class NovoRep {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Novo Rep ;)");
+		
+	}
+
+}
